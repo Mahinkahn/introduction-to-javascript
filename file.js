77594586bbf2fa09d8533f24a0f2388dd1ece7dc
Mahinkahn <mahinkahn37@gmail.com>
2022-08-31 12:38:30 +0600

    first commit

---
+++ file.js
@@ -0,0 +1,66 @@
+// js promlem 1
+var mom = 1000;
+// console.log(mom);
+
+var appel = 400;
+var orang = 300;
+var toter = appel + orang;
+// console.log(toter);
+var mom = 1000;
+var appel = 400;
+var orang = 300;
+var toter = appel + orang + mom;
+// console.log(toter);
+var toter = 700;
+var mom = 1000;
+var price = toter - mom;
+// console.log(price);
+
+var mom = 1000;
+var appel = 400;
+var orang = 300;
+var toter = mom - (appel + orang);
+console.log(toter)
+
+// js promlem 1 end 
+
+
+// js promlem 2 
+var bangla = 75.25;
+var englis = 65;
+var math = 80;
+var fajec = 35.45;
+var ict = 99.50;
+var mark = bangla + englis + math + fajec + ict;
+console.log(mark);
+var subject = 5;
+var toter = mark / subject;
+toter = toter.toFixed(2);
+toter = parseFloat(toter);
+console.log(toter);
+
+// js promlem 2 end 
+
+// js promlem 3 
+
+var usreName = 'I am going to be';
+var lastName = 'an awesome web developer';
+var fullName = usreName + ' ' + lastName;
+console.log(fullName);
+
+// js promlem 3 end 
+
+// js promlem 4 
+
+var first = 119;
+var reu = 5;
+var ans = first / reu;
+console.log(ans);
+var first = 119;
+var reu = 5;
+var ans = first % reu;
+console.log(ans);
+
+// js promlem 4 end 
+
+
---
+++ variable.js
@@ -0,0 +1,21 @@
+// vary + able = variable
+
+// numeric
+var price = 21;
+var age = 27;
+var temperature = 38;
+var whait = 62;
+var mane = 120;
+var busBera = 45;
+
+// string
+var person = "sodor uddin";
+var location = "andork killa bandorbon"
+var special = "alia bhatt"
+
+// Boolean
+var serious = true;
+var isFullMarks = true;
+var isSingle = false;
+
+
